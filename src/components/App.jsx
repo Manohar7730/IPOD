155ace0a5a8d0c182a16f28b22d38fed9ff3a553
Manohar <manohar20century@gmail.com>
2024-01-05 10:13:45 +0530

    completed the design of Ipod Case Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
 import { Component } from "react";
 import "../styles/App.css";
+import Case from "./Case";
+
 export default class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <>
+        <Case />
+      </>
+    );
   }
 }
---
+++ src/components/Case.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import "../styles/Case.css";
+
+export default class Case extends Component {
+  render() {
+    return (
+      <>
+        <div className="case">Case</div>
+      </>
+    );
+  }
+}
